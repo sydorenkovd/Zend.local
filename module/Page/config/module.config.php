a25546dc3dab9c88fbdbf802906827c23c5e9b38
sydorenkovd <sydorenkovd@gmail.com>
2015-11-05 12:02:10 +0200

    change config

--- module/Page/config/module.config.php
+++ module/Page/config/module.config.php
@@ -3,6 +3,25 @@
 namespace Application;
 
 return array(
+    'router' => array(
+        'routes' => array(
+            'page' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route'    => '/page[/:action][/:id]',
+                    'constraints'=>[
+                        'action'=>"[a-zA-Z0-9_-]*",
+                        'id'=>"[0-9]+",
+                    ],
+                    'defaults' => array(
+                        'controller' => 'Page\Controller\Page',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+
+            ),
+        ),
     'controllers' => array(
         'invokables' => array(
             'Page\Controller\Page' => Controller\PageController::class
